54c416ed092e0fce545c321d4a49a8ab36dac5da
Zalan888 <witmannz@kkszki.hu>
2024-10-18 10:23:06 +0200

    added input function

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,29 @@ namespace szamosszeg
 {
     internal class Program
     {
+        static int[] pos = new int[10];
+        static int[] neg = new int[10];
+        static int counter1 = 0;
+        static int counter2 = 0;
+        
+        static void input() {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Kérek egy számot!");
+               int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    neg[counter2] = num;
+                    counter2++;
+                }
+                else if (num > 0)
+                {
+                    pos[counter1] = num;
+                    counter1++;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
         }
